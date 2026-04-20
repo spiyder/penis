df49dac80cbf13ecf828e45a576dac5fecaf1910
spiyder <gergenov10@gmail.com>
2026-04-20 19:08:51 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,37 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["*.preview.same-app.com"],
   images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**', // Разрешить все пути на этом домене
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'another-site.org',
+        port: '',
+        pathname: '/uploads/**', // Разрешить только папку uploads
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
